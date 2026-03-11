a71fc43d4157abdcc69f799d39e367062de5e5d0
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2026-03-11 13:55:20 +0100

    feat: ark UI + initial Select

---
+++ components/Select.tsx
@@ -0,0 +1,60 @@
+import { Portal } from "@ark-ui/react/portal";
+import { Select, createListCollection } from "@ark-ui/react/select";
+import { ChevronsUpDownIcon, XIcon } from "lucide-react";
+import styles from "./index.module.css";
+
+const frameworks = createListCollection({
+  items: [
+    { label: "React", value: "react" },
+    { label: "Solid", value: "solid" },
+    { label: "Vue", value: "vue" },
+    { label: "Svelte", value: "svelte" },
+  ],
+});
+
+export const Basic = () => {
+  return (
+    <Select.Root className={styles.Root} collection={frameworks}>
+      <Select.Label className={styles.Label}>Framework</Select.Label>
+      <Select.Control className={styles.Control}>
+        <Select.Trigger className={styles.Trigger}>
+          <Select.ValueText className={styles.ValueText} placeholder="Select" />
+        </Select.Trigger>
+        <div className={styles.Indicators}>
+          <Select.ClearTrigger className={styles.ClearTrigger}>
+            <XIcon />
+          </Select.ClearTrigger>
+          <Select.Indicator className={styles.Indicator}>
+            <ChevronsUpDownIcon />
+          </Select.Indicator>
+        </div>
+      </Select.Control>
+      <Portal>
+        <Select.Positioner>
+          <Select.Content className={styles.Content}>
+            <Select.ItemGroup className={styles.ItemGroup}>
+              <Select.ItemGroupLabel className={styles.ItemGroupLabel}>
+                Frameworks
+              </Select.ItemGroupLabel>
+              {frameworks.items.map((item) => (
+                <Select.Item
+                  className={styles.Item}
+                  key={item.value}
+                  item={item}
+                >
+                  <Select.ItemText className={styles.ItemText}>
+                    {item.label}
+                  </Select.ItemText>
+                  <Select.ItemIndicator className={styles.ItemIndicator}>
+                    ✓
+                  </Select.ItemIndicator>
+                </Select.Item>
+              ))}
+            </Select.ItemGroup>
+          </Select.Content>
+        </Select.Positioner>
+      </Portal>
+      <Select.HiddenSelect />
+    </Select.Root>
+  );
+};
