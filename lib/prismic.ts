332f77804e9d35fbdfb3ef37615c5d211c083ff0
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2026-03-12 19:26:04 +0100

    chore: clean up

--- lib/prismic.ts
+++ lib/prismic.ts
@@ -11,16 +11,13 @@ export async function getBlogPosts(
   });
 
   const filters = [
-    // Ensure we only get blog posts
     prismic.filter.at("document.type", "blog_page"),
   ];
 
   if (category && category !== "all") {
-    // 1. Fetch the category document to get its internal Prismic ID
     const categoryDoc = await client.getByUID("category", category);
 
     if (categoryDoc) {
-      // 2. Filter using the ID (e.g., "abGrqxIAACEAFuqM")
       filters.push(prismic.filter.at("my.blog_page.category", categoryDoc.id));
     }
   }
@@ -29,7 +26,6 @@ export async function getBlogPosts(
     filters,
     pageSize: pagesize,
     page: pagination,
-    // This tells Prismic to "join" the category data so we have the name/label
     fetchLinks: ["category.name", "category.uid"],
     orderings: {
       field: "document.first_publication_date",
