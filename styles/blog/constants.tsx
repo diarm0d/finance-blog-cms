eec3b4cc40c89816e3326e2ea43ff52c7da5efa2
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2026-03-15 12:10:09 +0100

    chore: blog component styling

--- styles/blog/constants.tsx
+++ styles/blog/constants.tsx
@@ -12,12 +12,33 @@ export const blogComponents: RichTextComponents = {
       {children}
     </Heading>
   ),
+  heading3: ({ children }) => (
+    <Heading as="h3" size="sm" className={`mb-4`}>
+      {children}
+    </Heading>
+  ),
   paragraph: ({ children }) => (
     <p className={`text-md font-normal text-gray-500 mb-4`}>{children}</p>
   ),
   hyperlink: ({ children, node }) => (
-    <PrismicLink field={node.data} className="text-blue-500 hover:underline">
+    <PrismicLink field={node.data} className={`text-blue-500 hover:underline`}>
       {children}
     </PrismicLink>
+  ),
+  list: ({ children }) => (
+    <ul className={`list-disc ml-6 mb-4 space-y-2 text-gray-500`}>
+      {children}
+    </ul>
+  ),
+  listItem: ({ children }) => (
+    <li className={`text-md font-normal leading-relaxed`}>{children}</li>
+  ),
+  oList: ({ children }) => (
+    <ol className={`list-decimal ml-6 mb-4 space-y-2 text-gray-500`}>
+      {children}
+    </ol>
+  ),
+  oListItem: ({ children }) => (
+    <li className={`text-md font-normal leading-relaxed`}>{children}</li>
   ),
 };
